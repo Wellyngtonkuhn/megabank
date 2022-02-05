16441a317fdf89a7b587ebc32d330b8b55f8feca
Wellyngton Antonio Kuhn <94876959+Wellyngtonkuhn@users.noreply.github.com>
2022-02-04 21:29:30 -0300

    Banner finalizado

--- src/componentes/banner/Banner.js
+++ src/componentes/banner/Banner.js
@@ -1,21 +1,28 @@
-import { Card, Button } from 'react-bootstrap'
+import { Container, Row, Col, Card, Button, Image } from 'react-bootstrap'
+
 
 import './Banner.scss'
-import banner from '../../assets/banner.jpg'
+
+import banner_logo from '../../assets/banner_logo.png'
+import banner_logo_mobile from '../../assets/banner_logo_mobile.png'
 
 export default function Banner(){
 
 
     return(
         <>
-        <Card className="banner text-white">
-        <Card.Img src={banner} alt="banner image"/>
-        <Card.ImgOverlay>
-            <Card.Title>Pague suas contas pelo nosso APP</Card.Title>
-            
-            <Button variant="outline-light" className='bannerBtn'>Abra sua conta</Button>
-        </Card.ImgOverlay>
-        </Card>      
+        <Container fluid className="text-white banner text-center">
+            <Row className='py-lg-5' >
+                <Col lg className="my-lg-5" >
+                    <Image className='d-lg-none' src={banner_logo_mobile} alt='banner' />
+                    <Image className='d-none d-lg-inline-block' src={banner_logo} alt='banner' />
+                </Col>
+                <Col lg className="my-lg-5 banner_col_direita" >
+                    <p>Pague suas contas pelo nosso APP</p>
+                    <Button className=' btn bannerBtn' variant='outline-light'>Abra sua conta</Button>
+                </Col>
+            </Row>
+        </Container>      
         </>
     )
 } 
